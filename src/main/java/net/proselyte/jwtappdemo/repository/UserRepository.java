c8fd48e95b7a928e02e2874876365f6012e45d4b
cretuc <cristina.1998>
2021-10-17 19:51:57 +0300

    Initial commit

---
+++ src/main/java/net/proselyte/jwtappdemo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package net.proselyte.jwtappdemo.repository;
+
+import net.proselyte.jwtappdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String name);
+}
---
+++ src/test/java/net/proselyte/jwtappdemo/JwtappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselyte.jwtappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
